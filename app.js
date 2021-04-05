1e8ba2c1d69074415374c79aca832dda0aa16b7b
Anastasiya <nchulczova@gmail.com>
2021-04-05 23:55:57 +0300

    fix: replace tag selectors according to rule 3.4 in html-and-css.md and refacor: rename selectors according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 var taskInput=document.getElementById("task-adding-form__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("in-complete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var completedTasksHolder=document.getElementById("completed-tasks-list");//completed-tasks
 
 
 //New task list item
